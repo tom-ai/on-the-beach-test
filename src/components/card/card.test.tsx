714e14d478fe07f549270e44eccbb5622ac77d23
Tom Harris <tomharris3@me.com>
2024-03-16 19:49:43 +0000

    break the tests with an actual hotel

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -1,26 +1,38 @@
 import { render, screen } from '@testing-library/react';
-import Card from './Card.component';
+import Card from './card.component';
+import { Hotel } from '../../types/hotel';
+import { Booking } from '../../types/booking';
 
-it('should render an h2 for hotel name', () => {
-  render(<Card />);
+describe('Hotel information is displayed', () => {
+  const testHotel: Hotel = {
+    hotelName: 'Hotel Testing',
+    hotelLocation: 'Costa del Test',
+    starRating: 2,
+    overview:
+      'Welcome to the luxurious Azure Oasis Hotel, where dreams meet reality. Nestled amidst the rolling hills of nowhere, our hotel offers unparalleled tranquility and serenity. Enjoy our non-existent spa treatments and indulge in our imaginary gourmet cuisine, all while basking in the warmth of our nonexistent sunsets.',
+  };
 
-  const hotelName = screen.getByRole('heading', {
-    level: 2,
-    name: /hotel name/i,
+  it('should render an h2 for the hotel name', () => {
+    render(<Card />);
+
+    const hotelName = screen.getByRole('heading', {
+      level: 2,
+      name: testHotel.hotelName,
+    });
+
+    expect(hotelName).toBeVisible();
   });
 
-  expect(hotelName).toBeVisible();
-});
+  it('should render a paragraph below for the hotel location', () => {
+    render(<Card />);
 
-it('should render a paragraph below for the hotel location', () => {
-  render(<Card />);
+    const hotelName = screen.getByRole('heading', {
+      level: 2,
+      name: testHotel.hotelLocation,
+    });
 
-  const hotelName = screen.getByRole('heading', {
-    level: 2,
-    name: /hotel name/i,
+    const hotelLocation = hotelName.nextSibling;
+
+    expect(hotelLocation).toHaveTextContent(/hotel location/i);
   });
-
-  const hotelLocation = hotelName.nextSibling;
-
-  expect(hotelLocation).toHaveTextContent(/hotel location/i);
 });
