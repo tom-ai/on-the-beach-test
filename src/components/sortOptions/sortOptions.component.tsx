3267c0fe94a7de1c3c1b700707b1074b464405c4
Tom Harris <tomharris3@me.com>
2024-03-18 09:14:10 +0000

    refactor sortOptions component

--- src/components/sortOptions/sortOptions.component.tsx
+++ src/components/sortOptions/sortOptions.component.tsx
@@ -11,36 +11,35 @@ export default function SortOptions({
 }: SortOptionsProps) {
   // needs to return a sort state
 
-  return (
-    <>
-      <h1>Sort</h1>
-      <input
-        onChange={(e) => {
-          const value = e.target.value;
-          if (
-            value === 'alphabetical' ||
-            value === 'price' ||
-            value === 'star-rating'
-          ) {
-            handleSortOptionChange(value as SortOption);
-          }
-        }}
-        type="radio"
-        id="alphabetical"
-        value="alphabetical"
-        name="sort"
-      />
-      <label htmlFor="alphabetical">
-        Sort <strong>alphabetically</strong>
-      </label>
-      <input type="radio" id={sortOption} value={sortOption} name="sort" />
-      <label htmlFor={sortOption}>
-        Sort by <strong>price</strong>
-      </label>
-      <input type="radio" id="star-rating" value="star-rating" name="sort" />
-      <label htmlFor="star-rating">
-        Sort by <strong>star rating</strong>
-      </label>
-    </>
-  );
+  const sortOptions: SortOption[] = ['alphabetical', 'price', 'star-rating'];
+
+  sortOptions.map(() => {
+    return (
+      <>
+        <input
+          onChange={(e) => {
+            const value = e.target.value;
+            if (
+              value === 'alphabetical' ||
+              value === 'price' ||
+              value === 'star-rating'
+            ) {
+              handleSortOptionChange(value as SortOption);
+            }
+          }}
+          type="radio"
+          id={sortOption}
+          value={sortOption}
+          name="sort"
+        />
+        <label htmlFor={sortOption}>
+          {sortOption === 'alphabetical' &&
+            `Sort ${(<strong>alphabetically</strong>)}`}
+          {sortOption === 'price' && `Sort by ${(<strong>price</strong>)}`}
+          {sortOption === 'star-rating' &&
+            `Sort by ${(<strong>star rating</strong>)}`}
+        </label>
+      </>
+    );
+  });
 }
