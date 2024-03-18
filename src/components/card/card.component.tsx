0c7e212156c3a760cc2a98f27295244f5694ac6c
Tom Harris <tomharris3@me.com>
2024-03-18 16:00:42 +0000

    add chevron icons

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -41,6 +41,17 @@ export default function Card({ hotel, booking }: CardProps) {
           >
             <strong>Read {overviewHidden ? 'more' : 'less'}</strong> about this
             hotel
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              width="14px"
+              height="14px"
+              className={`inline-block ml-2 fill-blue-900 mb-0.5 ${
+                !overviewHidden && 'rotate-90'
+              }`}
+            >
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+            </svg>
           </button>
 
           <div className="p-4">
